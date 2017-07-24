95bd1d029537029fdc21d86607779d8e8d71d292
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-07-23 21:48:38 -0700

    updated pricing and JSX for list items

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor(){
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     //getInitialState if you're using ReactCreateClass
     this.state={
       fishes: {},
@@ -24,14 +27,26 @@ class App extends React.Component {
     //which state would you like to update?
     this.setState({ fishes })
   }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
   render(){
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+            <ul className="list-of-fishes">
+              {Object.keys(this.state.fishes)
+                      .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              }
+            </ul>
         </div>
         <Order />
-        <Inventory  addFish={this.addFish} />
+        <Inventory  addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
